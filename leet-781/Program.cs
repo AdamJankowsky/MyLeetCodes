099e892b78e340f10d09908d3c967bab0ba97885
Adam Jankowski <adam1jankowski@gmail.com>
2025-01-29 14:06:13 +0100

    Added LeetCode 781

---
+++ leet-781/Program.cs
@@ -0,0 +1,39 @@
+﻿var s = new Solution();
+int[] answers = [1, 1, 2];
+var result = s.NumRabbits(answers);
+Console.WriteLine(result);
+
+public class Solution
+{
+    public int NumRabbits(int[] answers)
+    {
+        var ans = new Dictionary<int, int>();
+        var sum = 0;
+        for (int i = 0; i < answers.Count(); ++i)
+        {
+            if (answers[i] == 0)
+            {
+                sum++;
+                continue;
+            }
+            var answer = answers[i];
+            if (!ans.ContainsKey(answer))
+            {
+                sum += answer + 1;
+                ans[answer] = 1;
+                continue;
+            }
+            if (ans[answer] < answer + 1)
+            {
+                ans[answer] += 1;
+                continue;
+            }
+            if (ans[answer] == answer + 1)
+            {
+                ans[answer] = 1;
+                sum += answer + 1;
+            }
+        }
+        return sum;
+    }
+}
