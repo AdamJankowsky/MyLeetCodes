09f53a4e018c244d1d64216227a14f866e813cab
Adam Jankowski <adam1jankowski@gmail.com>
2025-02-02 10:52:25 +0100

    Added leetcode 1752 3151

---
+++ leet-1752/Program.cs
@@ -0,0 +1,52 @@
+﻿var s = new Solution();
+
+int[] grid = [2, 4, 1, 3];
+
+var result = s.Check(grid);
+
+Console.WriteLine(result);
+
+
+public class Solution
+{
+    public bool Check(int[] nums)
+    {
+        bool secondPart = false;
+
+        for (int i = 0; i < nums.Length - 1; ++i)
+        {
+            if (!secondPart)
+            {
+                if (nums[i] <= nums[i + 1])
+                {
+                    continue;
+                }
+                else
+                {
+                    if (nums[i + 1] > nums[0])
+                    {
+                        return false;
+                    }
+                    secondPart = true;
+                    continue;
+                }
+            }
+            else
+            {
+                if (nums[i] <= nums[i + 1])
+                {
+                    if (nums[i + 1] > nums[0])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
---
+++ leet-3151/Program.cs
@@ -0,0 +1,28 @@
+﻿var s = new Solution();
+var result = s.IsArraySpecial([1, 5]);
+
+Console.WriteLine(result);
+
+public class Solution
+{
+    public bool IsArraySpecial(int[] nums)
+    {
+        if (nums.Length == 1)
+        {
+            return true;
+        }
+
+        var even = nums[0] % 2 == 0;
+        even = !even;
+        for (int i = 1; i < nums.Length; ++i)
+        {
+            var isEven = nums[i] % 2 == 0;
+            if (even != isEven)
+            {
+                return false;
+            }
+            even = !even;
+        }
+        return true;
+    }
+}
