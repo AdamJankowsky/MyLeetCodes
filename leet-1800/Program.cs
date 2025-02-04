1e4e2285399ad2a40137f03b8583c4b6137244a0
Adam Jankowski <adam1jankowski@gmail.com>
2025-02-04 08:42:51 +0100

    Added leetcode 1800

---
+++ leet-1800/Program.cs
@@ -0,0 +1,37 @@
+﻿var s = new Solution();
+
+int[] nums = [12, 17, 15, 13, 10, 11, 12];
+
+var result = s.MaxAscendingSum(nums);
+
+Console.WriteLine(result);
+
+public class Solution
+{
+    public int MaxAscendingSum(int[] nums)
+    {
+        if (nums.Length == 1)
+        {
+            return nums[0];
+        }
+
+        var partialSum = nums[0];
+        var maxSum = nums[0];
+
+        for (int i = 0; i < nums.Length - 1; ++i)
+        {
+            if (nums[i] < nums[i + 1])
+            {
+                partialSum += nums[i + 1];
+            }
+            else
+            {
+                maxSum = Math.Max(maxSum, partialSum);
+                partialSum = nums[i + 1];
+            }
+        }
+        maxSum = Math.Max(maxSum, partialSum);
+
+        return maxSum;
+    }
+}
