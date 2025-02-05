4ab3a4cd5ac160ce925353bd69eaff57124431a2
Adam Jankowski <adam1jankowski@gmail.com>
2025-02-05 08:57:12 +0100

    Added leetcode 1790

---
+++ leet-1790/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.WriteLine((new Solution()).AreAlmostEqual("bank", "kanb"));
+
+public class Solution
+{
+    public bool AreAlmostEqual(string s1, string s2)
+    {
+        var n = s1.Length;
+
+        int wrongCharsPtr = 0;
+        (char a, char b)[] wrongChars = new (char a, char b)[2];
+
+        for (int i = 0; i < n; ++i)
+        {
+            if (s1[i] == s2[i])
+            {
+                continue;
+            }
+            else
+            {
+                if (wrongCharsPtr == 2)
+                {
+                    return false;
+                }
+                wrongChars[wrongCharsPtr++] = (s1[i], s2[i]);
+            }
+        }
+
+        return wrongCharsPtr == 0 || (wrongChars[0].a, wrongChars[0].b) == (wrongChars[1].b, wrongChars[1].a);
+
+    }
+}
