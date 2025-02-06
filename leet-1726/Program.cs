ab024f01050b28dd4c6927e8c08bf1c344b4c6dd
Adam Jankowski <adam1jankowski@gmail.com>
2025-02-06 21:08:09 +0100

    Added leetcode 1726

---
+++ leet-1726/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.CompilerServices;
+
+var s = new Solution();
+int[] nums = [1, 2, 4, 5, 10];
+
+Console.WriteLine(s.TupleSameProduct(nums));
+
+
+public class Solution
+{
+    public int TupleSameProduct(int[] nums)
+    {
+        var n = nums.Length;
+
+        var products = new Dictionary<int, int>();
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (i == j)
+                {
+                    continue;
+                }
+
+                (int a, int b) = (nums[i], nums[j]);
+                var product = a * b;
+                if (products.ContainsKey(product))
+                {
+                    products[product] += 1;
+                }
+                else
+                {
+                    products[product] = 1;
+                }
+            }
+        }
+
+        return products.Values.Select(c =>
+        {
+            return (c * (c - 1)) - c;
+        }).Sum();
+    }
+}
