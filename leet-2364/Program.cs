a100feb2438bfca4ff255221c62077e86d957d29
Adam Jankowski <adam1jankowski@gmail.com>
2025-02-10 10:20:20 +0100

    Added leetcode 2364 and 3174

---
+++ leet-2364/Program.cs
@@ -0,0 +1,29 @@
+﻿var s = new Solution();
+
+Console.WriteLine(s.CountBadPairs([1, 2, 3, 4, 5]));
+
+public class Solution
+{
+    public long CountBadPairs(int[] nums)
+    {
+        var n = nums.Length;
+        long badPairs = 0;
+
+        var results = new Dictionary<int, int>();
+
+        for (int i = 0; i < n; ++i)
+        {
+            var result = nums[i] - i;
+            if (results.ContainsKey(result))
+            {
+                badPairs += i - results[result];
+                results[result]++;
+                continue;
+            }
+            badPairs += i;
+            results[result] = 1;
+        }
+
+        return badPairs;
+    }
+}
---
+++ leet-3174/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution {
+    public string ClearDigits(string s) {
+        var resultArr = new char[s.Length];
+        int resultsPtr = 0;
+        for(int i = 0; i<s.Length; ++i)
+        {
+            if(!Char.IsDigit(s[i]))
+            {
+                resultArr[resultsPtr++] = s[i];
+                continue;
+            }
+            if(resultsPtr == 0)
+            {
+                continue;
+            }
+            resultsPtr--;
+            continue;
+        }
+        var result = resultArr.AsSpan(0, resultsPtr);
+        return new string(result);
+    }
+}
