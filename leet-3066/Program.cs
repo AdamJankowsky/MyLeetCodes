91c004acd06f2a55a5656bb42e91966de81797fd
Adam Jankowski <adam1jankowski@gmail.com>
2025-02-13 11:45:56 +0100

    added leetcode 3066

---
+++ leet-3066/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+var s = new Solution();
+
+Console.WriteLine(s.MinOperations([999999999, 999999999, 999999999], 1000000000));
+
+
+public class Solution
+{
+    public int MinOperations(int[] nums, int k)
+    {
+        var pq = new PriorityQueue<long, long>();
+        for (int i = 0; i < nums.Length; ++i)
+        {
+            pq.Enqueue(nums[i], nums[i]);
+        }
+
+        var loops = 0;
+        while (pq.Peek() < k)
+        {
+            var first = pq.Dequeue();
+            var second = pq.Dequeue();
+
+            long el = first < second ? (first * 2) + second : (second * 2) + first;
+
+            pq.Enqueue(el, el);
+            loops++;
+        }
+
+        return loops;
+    }
+
+}
