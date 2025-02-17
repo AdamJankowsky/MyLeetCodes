a24e089c2b47d6e6a1e40086ca45aa9f410a75cb
Adam Jankowski <adam1jankowski@gmail.com>
2025-02-17 09:18:54 +0100

    added leetcode 1079

---
+++ leet-1079/Program.cs
@@ -0,0 +1,52 @@
+﻿
+var s = new Solution();
+
+Console.WriteLine(s.NumTilePossibilities("AAB"));
+
+public class Solution
+{
+    public int NumTilePossibilities(string tiles)
+    {
+        var nums = new Dictionary<char, int>();
+        for (int i = 0; i < tiles.Length; ++i)
+        {
+            var ch = tiles[i];
+            if (nums.ContainsKey(ch))
+            {
+                nums[ch]++;
+                continue;
+            }
+            nums[ch] = 1;
+        }
+
+        var possibilities = GetPossibleCombinations(nums);
+
+        return possibilities;
+    }
+
+    private int GetPossibleCombinations(Dictionary<char, int> nums)
+    {
+        if (!nums.Keys.Any())
+        {
+            return 0;
+        }
+
+        var sum = 0;
+        foreach (var val in nums.ToList())
+        {
+            var newNums = new Dictionary<char, int>(nums);
+            if (newNums[val.Key] == 1)
+            {
+                newNums.Remove(val.Key);
+            }
+            else
+            {
+                newNums[val.Key] -= 1;
+            }
+
+            sum += 1;
+            sum += GetPossibleCombinations(newNums);
+        }
+        return sum;
+    }
+}
