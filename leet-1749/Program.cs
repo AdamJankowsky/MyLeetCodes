45003c70eaf633fb2a2201078674253d73fe4a7c
Adam Jankowski <adam1jankowski@gmail.com>
2025-02-26 09:52:27 +0100

    added leetcode 1749

---
+++ leet-1749/Program.cs
@@ -0,0 +1,31 @@
+﻿var s = new Solution();
+Console.WriteLine(s.MaxAbsoluteSum([1, -3, 2, 3, -4]));
+
+public class Solution
+{
+    public int MaxAbsoluteSum(int[] nums)
+    {
+        if (nums.Length == 0)
+        {
+            return 0;
+        }
+
+        if (nums.Length == 1)
+        {
+            return Math.Abs(nums[0]);
+        }
+
+        var max = 0;
+        var min = 0;
+
+        var sum = 0;
+        for (int i = 0; i < nums.Length; ++i)
+        {
+            sum += nums[i];
+            max = Math.Max(max, sum);
+            min = Math.Min(min, sum);
+        }
+
+        return max - min;
+    }
+}
